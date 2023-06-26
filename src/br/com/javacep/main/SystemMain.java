3270cf9c55f463687c391075124d5c94c4297697
jeremiasfreitass <jeremiasjmf@hotmail.com>
2023-06-26 20:31:21 -0300

    first commit

---
+++ src/br/com/javacep/main/SystemMain.java
@@ -0,0 +1,7 @@
+package br.com.javacep.main;
+
+public class SystemMain {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/br/com/javacep/model/Address.java
@@ -0,0 +1,4 @@
+package br.com.javacep.model;
+
+public class Address {
+}
